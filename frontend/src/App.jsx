a6353c855f5e20d50c893de1a92bb36c2ed7869b
Your Name <compas.a.bsinfotech@gmail.com>
2025-04-27 12:20:38 +0800

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Register from "./Register";
+  import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+  import Register from "./Register";
 import Login from "./Login";
 import Dashboard from "./Dashboard";
 
